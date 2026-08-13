f29b8b9479ec94a806314e53f120021b5df5eca4
David <dcarlot@cglobal.fr>
2026-08-13 15:25:40 +0200

    dernière passe pour harmoniser aussi 00_ModeDeploiement.ps1 avec le même mécanisme automatique

--- usb-root/_CGLOBAL/PS1/00_ModeDeploiement.ps1
+++ usb-root/_CGLOBAL/PS1/00_ModeDeploiement.ps1
@@ -10,9 +10,8 @@ $ErrorActionPreference = 'Stop'
 # Configuration
 # ------------------------------------------------------------------
 
-$LogFile = "C:\_CGLOBAL\Logs\Log00_ModeDeploiement.txt"
-
 Import-Module "C:\_CGLOBAL\PS1\CGLOBAL.Common.psm1" -Force
+$LogFile = Get-CGlobalLogFile -ScriptPath $MyInvocation.MyCommand.Path
 Initialize-CGlobalLog -LogFile $LogFile
 
 # ------------------------------------------------------------------
